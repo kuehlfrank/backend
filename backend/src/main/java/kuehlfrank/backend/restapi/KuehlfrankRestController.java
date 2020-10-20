75c66cf7fbf95f0936c310af9005df654cce7633
CapOfCave <lars.kecker2009@gmail.com>
2020-10-20 22:20:21 +0200

    initialized rest interface

---
+++ backend/src/main/java/kuehlfrank/backend/restapi/KuehlfrankRestController.java
@@ -0,0 +1,14 @@
+package kuehlfrank.backend.restapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KuehlfrankRestController {
+
+	@GetMapping("/ping")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello, %s", name);
+	}
+}
