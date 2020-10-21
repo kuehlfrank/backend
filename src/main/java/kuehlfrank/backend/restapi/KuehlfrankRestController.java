4963d20c7567f781f4e954603be197474bbb08ee
Tom Stein <dev@tomstein.me>
2020-10-21 22:00:50 +0200

    add basic auth

--- src/main/java/kuehlfrank/backend/restapi/KuehlfrankRestController.java
+++ src/main/java/kuehlfrank/backend/restapi/KuehlfrankRestController.java
@@ -1,8 +1,6 @@
 package kuehlfrank.backend.restapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class KuehlfrankRestController {
@@ -16,6 +14,11 @@ public class KuehlfrankRestController {
 	public String fridge(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello, %s", name);
 	}
-	
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "That's pretty basic!";
+	}
 	
 }
