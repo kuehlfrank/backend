8ecb96052d01eda6603201661fb8d2c6555fe5cf
CapOfCave <lars.kecker2009@gmail.com>
2020-10-21 09:43:23 +0200

    Auto stash before merge of "auth" and "origin/auth"

--- backend/src/main/java/kuehlfrank/backend/JWTSecurityConfig.java
+++ backend/src/main/java/kuehlfrank/backend/JWTSecurityConfig.java
@@ -1,19 +1,35 @@
 package kuehlfrank.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
+@EnableWebSecurity
 public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests(authz -> authz
-				.antMatchers(HttpMethod.GET, "/foos/**").hasAuthority("SCOPE_read")
-				.antMatchers(HttpMethod.POST, "/foos").hasAuthority("SCOPE_write")
+				.antMatchers(HttpMethod.GET, "/ping").permitAll()
+//				.antMatchers(HttpMethod.POST, "/foos").hasAuthority("SCOPE_write")
 				.anyRequest().authenticated())
-				.oauth2ResourceServer(oauth2 -> oauth2.jwt());
+		.exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            )
+            .formLogin();
 	}
+	
+	@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
 }
--- src/main/java/kuehlfrank/backend/restapi/KuehlfrankRestController.java
+++ src/main/java/kuehlfrank/backend/restapi/KuehlfrankRestController.java
@@ -1,6 +1,11 @@
 package kuehlfrank.backend.restapi;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +20,12 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class KuehlfrankRestController {
 
+	@GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+	
+	
 	@GetMapping("/ping")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello, %s", name);
