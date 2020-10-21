ffb515ed35a85c6257b9ab2cd3cb185a8ba43567
Tom Stein <dev@tomstein.me>
2020-10-22 01:39:10 +0200

    oauth with Auth0

--- src/main/java/kuehlfrank/backend/SecurityConfiguration.java
+++ src/main/java/kuehlfrank/backend/SecurityConfiguration.java
@@ -1,28 +1,47 @@
 package kuehlfrank.backend;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
+                JwtDecoders.fromOidcIssuerLocation(issuer);
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**")
                 .authorizeRequests()
-                .antMatchers("/", "/login**").permitAll()
+                .antMatchers("/public").permitAll()
+                .antMatchers("/private").authenticated()
+                .antMatchers("/private-scoped").hasAuthority("SCOPE_read:messages")
                 .anyRequest().authenticated()
-                .and()
-                .oauth2Login();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("{noop}pass") // Spring Security 5 requires specifying the password storage format
-                .roles("USER");
+                .and().cors()
+                .and().oauth2ResourceServer().jwt();
     }
 }
--- src/main/java/kuehlfrank/backend/restapi/KuehlfrankRestController.java
+++ src/main/java/kuehlfrank/backend/restapi/KuehlfrankRestController.java
@@ -1,10 +1,12 @@
 package kuehlfrank.backend.restapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import kuehlfrank.backend.model.Message;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin(origins = "*")
 public class KuehlfrankRestController {
 
 	@GetMapping("/ping")
@@ -15,5 +17,20 @@ public class KuehlfrankRestController {
 	@GetMapping("/inventory")
 	public String fridge(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello, %s", name);
+	}
+
+	@GetMapping(value = "/public")
+	public Message publicEndpoint() {
+		return new Message("All good. You DO NOT need to be authenticated to call /api/public.");
+	}
+
+	@GetMapping(value = "/private")
+	public Message privateEndpoint() {
+		return new Message("All good. You can see this because you are Authenticated.");
+	}
+
+	@GetMapping(value = "/private-scoped")
+	public Message privateScopedEndpoint() {
+		return new Message("All good. You can see this because you are Authenticated with a Token granted the 'read:messages' scope");
 	}
 }
