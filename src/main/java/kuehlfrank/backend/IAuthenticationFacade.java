f5ae3b676af45316a0987fe7d49179f97c24078e
Tom Stein <dev@tomstein.me>
2020-10-23 02:31:01 +0200

    add currentUser Endpoint

---
+++ src/main/java/kuehlfrank/backend/IAuthenticationFacade.java
@@ -0,0 +1,7 @@
+package kuehlfrank.backend;
+
+import org.springframework.security.core.Authentication;
+
+public interface IAuthenticationFacade {
+    Authentication getAuthentication();
+}
--- src/main/java/kuehlfrank/backend/restapi/TestController.java
+++ src/main/java/kuehlfrank/backend/restapi/TestController.java
@@ -1,19 +1,24 @@
 package kuehlfrank.backend.restapi;
 
+import kuehlfrank.backend.IAuthenticationFacade;
+import kuehlfrank.backend.model.Message;
 import kuehlfrank.backend.model.ServerInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import kuehlfrank.backend.model.Message;
-
 @RestController
 @RequestMapping(path = "test", produces = MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin(origins = "*")
 public class TestController {
+
+    @Autowired
+    private IAuthenticationFacade authenticationFacade;
 
     @Value("${spring.profiles.active:Unknown}")
     private String activeProfile;
@@ -26,6 +31,12 @@ public class TestController {
     @GetMapping(value = "/public")
     public Message publicEndpoint() {
         return new Message("All good. You DO NOT need to be authenticated to call /api/public.");
+    }
+
+    @GetMapping(value ="/currentUser")
+    public Message currentUser() {
+        Authentication authentication = authenticationFacade.getAuthentication();
+        return new Message(authentication.getName());
     }
 
     @GetMapping(value = "/private")
