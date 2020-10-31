bc799d8dc383f778fd0313cf0d902526860a4d79
CapOfCave <lars.kecker2009@gmail.com>
2020-10-31 23:25:37 +0100

    fixed unit order

--- src/main/java/kuehlfrank/backend/repositories/UnitRepository.java
+++ src/main/java/kuehlfrank/backend/repositories/UnitRepository.java
@@ -3,12 +3,14 @@ package kuehlfrank.backend.repositories;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import kuehlfrank.backend.model.Unit;
 
 public interface UnitRepository extends CrudRepository<Unit, UUID>{
 
+	@Query("SELECT u from Unit u ORDER BY LOWER(u.label)")
     public List<Unit> findAllByOrderByLabelAsc();
 
 }
